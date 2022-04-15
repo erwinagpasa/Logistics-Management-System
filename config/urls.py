6ddf32581af5fd6f24d148cc86a56a0d6a4154ed
erwinagpasa <istran.net@gmail.com>
2022-04-15 12:10:58 +0300

    static files

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
-
-# from django.contrib.auth import views as auth_views
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
   path('', include('core.urls')), #Match everything in core app
   path('admin/', admin.site.urls),
   path('accounts/', include('allauth.urls')),  
 ]
+# Serving static files
+# urlpatterns += staticfiles_urlpatterns()
